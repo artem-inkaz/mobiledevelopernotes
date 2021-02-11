9c3ce7a9553fefe9cb3a28e6fa36331935c04754
artemternovenko <artem.inkaz@gmail.com>
2021-02-11 18:36:33 +0600

    fill Main Activity

---
+++ app/src/main/java/com/example/mobiledevelopernotes/utilits/constants.kt
@@ -0,0 +1,6 @@
+package com.example.mobiledevelopernotes.utilits
+
+import com.example.mobiledevelopernotes.MainActivity
+
+// объявляем глобальные переменные
+lateinit var APP_ACTIVITY: MainActivity
