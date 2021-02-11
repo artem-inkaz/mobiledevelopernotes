0ee3601ddb61a8be622c9380cb51dcaa0f0b7722
artemternovenko <artem.inkaz@gmail.com>
2021-02-11 18:06:13 +0600

    add viewBinding in gradle

--- app/src/main/java/com/example/mobiledevelopernotes/MainActivity.kt
+++ app/src/main/java/com/example/mobiledevelopernotes/MainActivity.kt
@@ -2,8 +2,16 @@ package com.example.mobiledevelopernotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
